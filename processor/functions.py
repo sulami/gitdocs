50bb5ad60594062831b3426180227b3b26c121bb
Robin Schroer <sulamiification@gmail.com>
2014-05-08 15:52:46 +0200

    added process_docs function prototype

---
+++ processor/functions.py
@@ -0,0 +1,3 @@
+def process_docs(instr):
+    return('<h1>Test</h1>')
+
--- processor/tests.py
+++ processor/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from processor.functions import process_docs
 
-# Create your tests here.
+class ProcessDocs(TestCase):
+    def test_readme_gets_processed(self):
+        docs = process_docs('# Test')
+        self.assertEqual(docs, '<h1>Test</h1>')
+
