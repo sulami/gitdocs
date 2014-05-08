45c33ad2b43e0ba50d855839c56552855bf32f26
Robin Schroer <sulamiification@gmail.com>
2014-05-08 16:33:09 +0200

    changed tests to use global instances

--- gitdocs/processor/tests.py
+++ gitdocs/processor/tests.py
@@ -3,29 +3,30 @@ from processor.functions import process_docs
 from processor.models import Docs, Version
 
 class ProcessDocsTestCase(TestCase):
+    def setUp(self):
+        docs = Docs()
+        docs.owner = 'sulami'
+        docs.name = 'Test'
+        docs.save()
+        ver = Version()
+        ver.name = 'master'
+        ver.content = '# Test'
+        ver.save()
+
     def test_readme_gets_processed(self):
         docs = process_docs('# Test')
         self.assertEqual(docs, '<h1>Test</h1>')
 
     def test_docs_model(self):
-        docs = Docs()
-        self.assertIsNotNone(docs)
+        self.assertIsNotNone(Docs.objects.get(pk=1))
 
     def test_docs_model_holds_data(self):
-        tmp = Docs()
-        tmp.owner = 'sulami'
-        tmp.name = 'Test'
-        tmp.save()
         docs = Docs.objects.get(pk=1)
         self.assertEqual(docs.owner, 'sulami')
         self.assertEqual(docs.name, 'Test')
 
     def test_version_model_holds_data(self):
-        tmp = Version()
-        tmp.name = '0.1'
-        tmp.content = '# Test'
-        tmp.save()
         ver = Version.objects.get(pk=1)
-        self.assertEqual(ver.name, '0.1')
+        self.assertEqual(ver.name, 'master')
         self.assertEqual(ver.content, '# Test')
 
