f321f21b68d65728cd1452bc1fcd25fcba94fbcc
Robin Schroer <sulamiification@gmail.com>
2014-05-08 19:23:29 +0200

    added getdocs app

---
+++ gitdocs/getdocs/functions.py
@@ -0,0 +1,10 @@
+from processor.models import Docs
+
+def get_docs(owner, repo):
+    print owner, repo
+    try:
+        qset = Docs.objects.get(owner=owner, name=repo)
+    except Docs.DoesNotExist:
+        qset = None
+    return qset
+
---
+++ gitdocs/getdocs/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from getdocs.functions import get_docs
+from processor.models import Docs, Version
+
+class GetDocsTestCase(TestCase):
+    def setUp(self):
+        self.docs = Docs(owner='sulami', name='Test')
+        self.docs.save()
+        self.verm = Version(name='master', content='# Test')
+        self.verm.save()
+        self.ver1 = Version(name='0.1', content='# Not master')
+        self.ver1.save()
+        self.ver2 = Version(name='0.2', content='# Not really master')
+        self.ver2.save()
+        self.docs.versions.add(self.verm)
+        self.docs.versions.add(self.ver1)
+        self.docs.versions.add(self.ver2)
+
+    def tearDown(self):
+        for doc in Docs.objects.all():
+            doc.delete()
+        for ver in Version.objects.all():
+            ver.delete()
+
+    def test_get_existing_docs(self):
+        docs = get_docs('sulami', 'Test')
+        self.assertIsNotNone(docs)
+
+    # def test_get_not_existing_docs(self):
+        # notdocs = get_docs('foo', 'bar')
+        # self.assertIsNone(notdocs)
+
