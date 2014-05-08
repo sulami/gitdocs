2f3b19f21665e471d7460d6a247f50948c6739a3
Robin Schroer <sulamiification@gmail.com>
2014-05-08 16:01:01 +0200

    changed test case name

--- processor/tests.py
+++ processor/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from processor.functions import process_docs
 
-class ProcessDocs(TestCase):
+class ProcessDocsTestCase(TestCase):
     def test_readme_gets_processed(self):
         docs = process_docs('# Test')
         self.assertEqual(docs, '<h1>Test</h1>')
