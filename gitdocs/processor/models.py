0f499e4485be8184b97f2aa508459941cd1b1fdc
Robin Schroer <sulamiification@gmail.com>
2014-05-08 16:29:44 +0200

    added version model

--- gitdocs/processor/models.py
+++ gitdocs/processor/models.py
@@ -4,3 +4,7 @@ class Docs(models.Model):
     owner = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
 
+class Version(models.Model):
+    name = models.CharField(max_length=30)
+    content = models.TextField()
+
--- gitdocs/processor/tests.py
+++ gitdocs/processor/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from processor.functions import process_docs
-from processor.models import Docs
+from processor.models import Docs, Version
 
 class ProcessDocsTestCase(TestCase):
     def test_readme_gets_processed(self):
@@ -19,4 +19,13 @@ class ProcessDocsTestCase(TestCase):
         docs = Docs.objects.get(pk=1)
         self.assertEqual(docs.owner, 'sulami')
         self.assertEqual(docs.name, 'Test')
+
+    def test_version_model_holds_data(self):
+        tmp = Version()
+        tmp.name = '0.1'
+        tmp.content = '# Test'
+        tmp.save()
+        ver = Version.objects.get(pk=1)
+        self.assertEqual(ver.name, '0.1')
+        self.assertEqual(ver.content, '# Test')
 
