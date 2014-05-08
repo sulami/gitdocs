e83cb5ec95acd5efa5d006552eeafbe4517f3c3a
Robin Schroer <sulamiification@gmail.com>
2014-05-08 16:05:09 +0200

    added processor/models/Docs model

--- processor/tests.py
+++ processor/tests.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from processor.functions import process_docs
+from processor.models import Docs
 
 class ProcessDocsTestCase(TestCase):
     def test_readme_gets_processed(self):
         docs = process_docs('# Test')
         self.assertEqual(docs, '<h1>Test</h1>')
+
+    def test_doc_model(self):
+        docs = Docs()
+        self.assertIsNotNone(docs)
 
