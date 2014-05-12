1cbccdc1ce3fad344102a0bf383950a2761ccfcf
Robin Schroer <sulamiification@gmail.com>
2014-05-12 11:03:28 +0200

    fixed dots in tags throwing regex exceptions

--- renderer/urls.py
+++ renderer/urls.py
@@ -3,7 +3,7 @@ from renderer.views import *
 
 urlpatterns = patterns('renderer.views',
     url(r'^$', 'index', name='index'),
-    url(r'(?P<username>[\w]+)/(?P<reponame>[\w]+)/(?P<versionname>[\w]+)/$',
+    url(r'(?P<username>[\w]+)/(?P<reponame>[\w]+)/(?P<versionname>[\w.]+)/$',
         'version', name='version'),
     url(r'(?P<username>[\w]+)/(?P<reponame>[\w]+)/$', 'repo', name='repo'),
 )
